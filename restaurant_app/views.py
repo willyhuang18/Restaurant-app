34f609e7cbe79e5dc7454da2e404a62bd130543b
Willy Huang <willyhuang18@gmail.com>
2023-11-13 12:01:12 -0800

    rendering the data from views to html

--- restaurant_app/views.py
+++ restaurant_app/views.py
@@ -5,6 +5,13 @@ from .models import Item
 class MenuList(generic.ListView):
     queryset = Item.objects.order_by('date_created')
     template_name = "index.html"
+    
+    def get_context_data(self):
+        context = {'meals':['Pizza','Pasta'],
+                   'drinks':['Coffee', 'Tea'],
+                   'ingredients':['things']}
+        
+        return context
 
 class MenuItemDetail(generic.DetailView):
     model = Item
