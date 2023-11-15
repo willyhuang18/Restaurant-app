7a83b453ef5db4e17fbb844b18979d7cb5174800
Willy Huang <willyhuang18@gmail.com>
2023-11-14 23:21:51 -0800

    using the jinja for loop in the html page to load the data

--- restaurant_app/views.py
+++ restaurant_app/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Item
+from .models import Item, MEAL_TYPE
 # Create your views here.
 class MenuList(generic.ListView):
     queryset = Item.objects.order_by('date_created')
     template_name = "index.html"
-    
-    def get_context_data(self):
-        context = {'meals':['Pizza','Pasta'],
-                   'drinks':['Coffee', 'Tea'],
-                   'ingredients':['things']}
+    # special parameter
+    def get_context_data(self,**kwargs):
+        # call the parent class(listView), access get_context_data
+        # an existing dictionary from the ListView class 
+        # get some default values already. 
+        # this will be a dictionary,
+        context = super().get_context_data(**kwargs)
+        context['meals'] = MEAL_TYPE
         
         return context
 
