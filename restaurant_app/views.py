8f48a47fa5e67763a86b08b58e234ca5d27c163a
willyhuang18 <willyhuang18@gmail.com>
2023-11-21 14:27:16 -0800

    adding the review for each item detail

--- restaurant_app/views.py
+++ restaurant_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Item, MEAL_TYPE,Review
+from .form import ReviewForm
 # created two class-based views.
 #  the page will be represented by the MenuList class ListView
 class MenuList(generic.ListView):
@@ -23,3 +24,17 @@ class MenuItemDetail(generic.DetailView):
     
     model = Item
     template_name = 'menu_item_detail.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = ReviewForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.item = self.get_object()
+            review.user = request.user
+            review.save()
+        return super().get(request, *args, **kwargs)
